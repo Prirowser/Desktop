dfa6f14ce69b4d6c0837a3f7747ca4d427581996
zalnaRs <thezalnar@hotmail.com>
2020-08-04 10:38:17 +0200

    💣 Add "electron ." script and main process

---
+++ src/electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        title: 'Prirowser',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('../html/index.html');
+
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+})
