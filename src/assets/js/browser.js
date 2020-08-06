de410ba6f28a1eadff75207462b6eaff0e096ee1
zalnaRs <thezalnar@hotmail.com>
2020-08-06 07:46:11 +0200

    :fast_forward: Added urlbar

--- src/assets/js/browser.js
+++ src/assets/js/browser.js
@@ -1,11 +1,21 @@
 function back() {
-    if (document.querySelector('webview').canGoBack) {
-        document.querySelector('webview').goBack();
-    }
+    document.getElementById('webview').goBack();
 }
 
 function forward() {
-    if (document.querySelector('webview').canGoForward) {
-        document.querySelector('webview').goForward();
-    }
+    document.getElementById('webview').goForward();
 }
+
+document.getElementById('urlbar').addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        let https = document.getElementById('urlbar').value.slice(0, 8).toLowerCase();
+        let http = document.getElementById('urlbar').value.slice(0, 7).toLowerCase();
+        if (https === 'https://') {
+            document.getElementById('webview').loadURL(document.getElementById('urlbar').value);
+        } else if (http === 'http://') {
+            document.getElementById('webview').loadURL(document.getElementById('urlbar').value);
+        } else {
+            document.getElementById('webview').loadURL('http://' + document.getElementById('urlbar').value);
+        }
+    }
+});
